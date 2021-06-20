dfaef662acc0cee8e16a0c0bbe8b6a23318c25b6
Pedro Plumer <p.plumer@sap.com>
2021-06-20 17:33:33 -0300

    file structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 );
+
+
+// client side rendered app: react (cra)
+  // -> database = firebase
+  // -> react-loading-skeleton
+  // -> style = tailwind
+
+// folder structure
+  // src
+    // -> components
+    // -> constants
+    // -> context
+    // -> helpers
+    // -> hooks
+    // -> pages
+    // -> lib (firebase in here)
+    // -> service (firebase functions)
+    // -> styles (tailwind´s folder (app/tailwind))
 
