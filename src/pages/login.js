f2feecb138e36d15ce7e1366bceaed7268df3740
Pedro Plumer <p.plumer@sap.com>
2021-06-21 11:06:34 -0300

    tailwind setup

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,7 +17,16 @@ const Login = () => {
         document.title = 'Login - Instagram';
     }, [])
 
-    return <p>Hi login</p>
+    return (
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex w-3/5">
+                <img src="/images/iphone-with-profile.jpg" alt="Iphone with Instagram App"/>
+            </div>
+            <div className="flex flex-col w-2/5">
+                <p>fORM</p>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
